73f823ac785ad97a2d3d1bc2eb33d65445e2e0df
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-12-15 17:38:30 -0500

    BUG: Fix branch name for push event

--- cancel_workflows.py
+++ cancel_workflows.py
@@ -3,10 +3,11 @@ import os
 from github import Github
 
 gh_ref = os.environ['GITHUB_REF']
+branch_name = gh_ref.replace('refs/heads/', '')
 repo_name = os.environ['GITHUB_REPOSITORY']
 g = Github(os.environ.get('GITHUB_TOKEN'))
 repo = g.get_repo(repo_name)
-workflows = repo.get_workflow_runs(branch=gh_ref)
+workflows = repo.get_workflow_runs(branch=branch_name)
 
 for wf in workflows:
     if wf.status != 'completed':
