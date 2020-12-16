311c223a1f36f96d06be5662eae5ebb5ddecdb42
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-12-15 20:37:00 -0500

    Improve workflow cancel logic

--- cancel_workflows.py
+++ cancel_workflows.py
@@ -19,9 +19,9 @@ workflows_running = repo.get_workflow_runs(branch=branch_name,
 workflows_queued = repo.get_workflow_runs(branch=branch_name, status='queued')
 
 latest_timestamp = None
-workflows_list = [workflows_queued, workflows_running]
+workflows_list = []
 
-for wlist in workflows_list:
+for wlist in (workflows_queued, workflows_running):
     for wf in wlist:
         wid = wf.workflow_id
         if wid in workflow_lookup_cache:
@@ -31,10 +31,13 @@ for wlist in workflows_list:
             workflow_lookup_cache[wid] = wname
         if wname != workflow_name:
             continue
+        workflows_list.append(wf)
         if latest_timestamp is None:
             latest_timestamp = wf.created_at
         elif wf.created_at > latest_timestamp:
             latest_timestamp = wf.created_at
+        print('Found {wf} created at {wf.created_at}, '
+              f'latest timestamp now {latest_timestamp}')
 
 if latest_timestamp is None:
     print(f'No duplicate workflows for {workflow_name} workflow for '
@@ -42,11 +45,11 @@ if latest_timestamp is None:
           f'lookup: {workflow_lookup_cache}')
     sys.exit(0)
 
-for wlist in workflows_list:
-    for wf in wlist:
-        if wf.created_at < latest_timestamp:
-            print(f'Cancelling {wf.status} {wf}')
-            wf.cancel()
+for wf in workflows_list:
+    if wf.created_at < latest_timestamp:
+        print(f'Cancelling {wf.status} {wf}')
+        wf.cancel()
 
 print(f'Done checking for duplicate workflows for {workflow_name} for '
-      f'{branch_name} branch in {repo_name}')
+      f'{branch_name} branch in {repo_name}\n\n'
+      f'lookup: {workflow_lookup_cache}')
