01ef161320cecc2a6981ddc1183ca8b9e36b5bae
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-12-15 19:41:50 -0500

    BUG: Check for run ID too

--- cancel_workflows.py
+++ cancel_workflows.py
@@ -3,6 +3,7 @@ import sys
 
 from github import Github
 
+workflow_id = os.environ['GITHUB_RUN_ID']
 event_name = os.environ['GITHUB_EVENT_NAME']
 if event_name in ('pull_request_target', 'pull_request'):
     branch_name = os.environ['GITHUB_HEAD_REF']
@@ -17,32 +18,27 @@ workflows_running = repo.get_workflow_runs(branch=branch_name,
 workflows_queued = repo.get_workflow_runs(branch=branch_name, status='queued')
 
 latest_timestamp = None
+workflows_list = [workflows_queued, workflows_running]
 
-for wf in workflows_queued:
-    if latest_timestamp is None:
-        latest_timestamp = wf.created_at
-    elif wf.created_at > latest_timestamp:
-        latest_timestamp = wf.created_at
-
-for wf in workflows_running:
-    if latest_timestamp is None:
-        latest_timestamp = wf.created_at
-    elif wf.created_at > latest_timestamp:
-        latest_timestamp = wf.created_at
+for wlist in workflows_list:
+    for wf in wlist:
+        if wf.workflow_id != workflow_id:
+            continue
+        if latest_timestamp is None:
+            latest_timestamp = wf.created_at
+        elif wf.created_at > latest_timestamp:
+            latest_timestamp = wf.created_at
 
 if latest_timestamp is None:
-    print(f'No duplicate workflows for {branch_name} in {repo_name}')
+    print(f'No duplicate workflows for ID {workflow_id} for {branch_name} in '
+          f'{repo_name}')
     sys.exit(0)
 
-for wf in workflows_running:
-    if wf.created_at < latest_timestamp:
-        print(f'Cancelling in-progress {wf}')
-        wf.cancel()
+for wlist in workflows_list:
+    for wf in wlist:
+        if wf.created_at < latest_timestamp:
+            print(f'Cancelling {wf.status} {wf}')
+            wf.cancel()
 
-for wf in workflows_queued:
-    if wf.created_at < latest_timestamp:
-        print(f'Cancelling queued {wf}')
-        wf.cancel()
-
-print(f'Done checking for duplicate workflows for {branch_name} in '
-      '{repo_name}')
+print(f'Done checking for duplicate workflows for ID {workflow_id} for '
+      f'{branch_name} in {repo_name}')
